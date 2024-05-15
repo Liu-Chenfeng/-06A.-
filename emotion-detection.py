00ac9b43dcd13b7add62efb88b14002dc01d1a73
Liu-Chenfeng <162109174+Liu-Chenfeng@users.noreply.github.com>
2024-05-15 22:38:08 +1000

    emotion-detection.py

--- emotion-detection.py
+++ emotion-detection.py
@@ -1,43 +1,40 @@
-### 1 数据准备
+### 1 数据准备   Подготовка данных
 import pandas as pd
 
-## 1.1 数据读取
-df = pd.read_csv("C:\\Users\\lcf14\\Desktop\\emotion_dataset_raw.csv")
+## 1.1 数据读取   Чтение данных
+df = pd.read_csv("emotion_dataset_raw.csv")
 
-## 1.2 数据探索
-#print(df.head())
-#print(df['Emotion'].value_counts())
+## 1.2 数据探索   Просмотр данных
+print(df.head())
+print(df['Emotion'].value_counts())
 
-## 1.3 数据预处理  (使用neattext库)
+## 1.3 数据预处理  (使用neattext库)   Предварительная обработка данных (с помощью библиотеки neattext)
 import neattext.functions as nfx
 
-    # 去除用户信息
+    # 去除用户信息   Удаление информации о пользователе
 df['Clean_Text'] = df['Text'].apply(nfx.remove_userhandles)
 
-    # 去除句中的停用词
+    # 去除句中的停用词   Удаление стоп-слов из предложений
 df['Clean_Text'] = df['Clean_Text'].apply(nfx.remove_stopwords)
-#print(df.head())
 
-    # 文本向量化 (使用词袋模型) (另一种常用的方法为TF-IDF)
+    # 文本向量化 (使用词袋模型) (另一种常用的方法为TF-IDF) 
+    # Векторизация текста (с использованием моделей "мешка слов") (другой распространенный метод - TF-IDF)
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-
+#from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = CountVectorizer(max_features=1000)
 X = vectorizer.fit_transform(df['Clean_Text'])
 
-    # 将情感标签转换为数字
+    # 将情感标签转换为数字   Преобразование эмоциональных меток в цифры
 emotion_mapping = {'neutral': 0, 'joy': 1, 'sadness': 2, 'fear': 3, 'surprise': 4, 'anger': 5, 'shame': 6, 'disgust': 7}
 y = df['Emotion'].map(emotion_mapping)
-#print(y)
 
-    # 划分训练集和测试集
+    # 划分训练集和测试集   Различайте обучающие и тестовые наборы
 from sklearn.model_selection import train_test_split
-
 X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
 
-### 2 模型训练
+### 2 模型训练   Обучение модели
 
 ## 2.1 OneRule  （如果运行这个算法，需要更改vectorizer的特征值，特征值为10需要30秒，为100需要四分钟）
 import numpy as np
@@ -49,41 +46,34 @@ def oneR_classifier(X_train, y_train, X_test):
     best_accuracy = 0
     best_predictions = None
     num_features = X_train.shape[1]
-
     for feature_index in trange(num_features, desc='Finding best feature'):
-        # 提取每个特征的所有值
+        # 提取每个特征的所有值 Извлеките все значения для каждого признака
         feature_values = X_train[:, feature_index].toarray().ravel()
-
-        # 基于这个特征创建一个简单的预测模型
+        # 基于这个特征创建一个预测模型 Создайте предсказательную модель на основе этого признака
         predictions = {}
         for unique_value in np.unique(feature_values):
             if unique_value != 0:
                 mask = feature_values == unique_value
                 most_common_class = np.bincount(y_train[mask]).argmax()
                 predictions[unique_value] = most_common_class
-
-        # 使用这个特征进行预测
+        # 使用这个特征进行预测 Используйте это признак для предсказаний
         pred_labels = np.array([predictions.get(val, np.random.choice(np.unique(y_train))) for val in X_test[:, feature_index].toarray().ravel()])
-
-        # 计算准确率
+        # 计算准确率 Точность вычислений
         accuracy = accuracy_score(y_test, pred_labels)
-
-        # 更新最佳特征
+        # 更新最佳特征 Обновление лучшего признака
         if accuracy > best_accuracy:
             best_accuracy = accuracy
             best_feature = feature_index
             best_predictions = pred_labels
-
     return best_feature, best_accuracy, best_predictions
 
-    # 运行oneR，获取最佳特征和准确度
+    # 运行oneR，获取最佳特征和准确度  Получение наилучших признаки и точности
 best_feature, best_accuracy, best_predictions = oneR_classifier(X_train, y_train, X_test)
-print(f"OneR_Accuracy: {best_accuracy}")   # OneR_Accuracy: 0.1431240120706998
+print(f"OneR_Accuracy: {best_accuracy}")     # OneR_Accuracy: 0.1431240120706998
 
-    # 获取最佳特征对应的单词
+    # 获取最佳特征对应的单词 Получите слово, соответствующее лучшей признаку
 best_feature_name = vectorizer.get_feature_names_out()[best_feature]
 print(f"The best feature by OneR is: {best_feature_name}")
-
 
 
 
@@ -99,9 +89,9 @@ LR_y_pred = LR_model.predict(X_test)
     # 计算准确率
 from sklearn.metrics import accuracy_score
 LR_accuracy = accuracy_score(y_test, LR_y_pred)
-print(f"LR_Accuracy: {LR_accuracy}")   # 准确率为：0.5640178186521052
-"""
-    # 使用混淆矩阵来评估结果
+print(f"LR_Accuracy: {LR_accuracy}")     # LR_accuracy：0.5640178186521052
+
+    # 使用混淆矩阵来评估结果 Используйте матрицу путаницы для оценки результатов
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
@@ -114,7 +104,6 @@ plt.xlabel('Predicted Label')
 plt.ylabel('True Label')
 plt.title('Confusion Matrix')
 plt.show()
-"""
 
 
 
@@ -130,8 +119,8 @@ KNN_y_pred = KNN_model.predict(X_test)
     # 计算准确率
 from sklearn.metrics import accuracy_score
 KNN_accuracy = accuracy_score(y_test, KNN_y_pred)
-print(f"KNN_Accuracy: {KNN_accuracy}")      # 准确率为：0.4227618910763041
-"""
+print(f"KNN_Accuracy: {KNN_accuracy}")     # KNN_accuracy：0.4227618910763041
+
     # 使用混淆矩阵来评估结果
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
@@ -145,7 +134,7 @@ plt.xlabel('Predicted Label')
 plt.ylabel('True Label')
 plt.title('Confusion Matrix')
 plt.show()
-"""
+
 
 
 
@@ -161,8 +150,8 @@ NB_y_pred = NB_model.predict(X_test)
     # 计算准确率
 from sklearn.metrics import accuracy_score
 NB_accuracy = accuracy_score(y_test, NB_y_pred)
-print(f"NB_Accuracy: {NB_accuracy}")        # 准确率为：0.5349906595775255
-"""
+print(f"NB_Accuracy: {NB_accuracy}")     # NB_accuracy：0.5349906595775255
+
     # 使用混淆矩阵来评估结果
 from sklearn.metrics import roc_curve, roc_auc_score, confusion_matrix
 import matplotlib.pyplot as plt
@@ -174,7 +163,6 @@ plt.xlabel('Predicted labels')
 plt.ylabel('True labels')
 plt.title('Confusion Matrix')
 plt.show()
-"""
 
 
 
@@ -190,8 +178,8 @@ DT_y_pred = DT_model.predict(X_test)
     # 计算准确率
 from sklearn.metrics import accuracy_score
 DT_accuracy = accuracy_score(y_test, DT_y_pred)
-print(f"DT_Accuracy: {DT_accuracy}")       # 准确率为：0.5082626814197442
-"""
+print(f"DT_Accuracy: {DT_accuracy}")     # DT_accuracy：0.5082626814197442
+
     # 使用混淆矩阵来评估结果
 from sklearn.metrics import roc_curve, roc_auc_score, confusion_matrix
 import matplotlib.pyplot as plt
@@ -203,7 +191,6 @@ plt.xlabel('Predicted labels')
 plt.ylabel('True labels')
 plt.title('Confusion Matrix')
 plt.show()
-"""
 
 
 
@@ -219,8 +206,8 @@ GB_y_pred = GB_model.predict(X_test)
     # 计算准确率
 from sklearn.metrics import accuracy_score
 GB_accuracy = accuracy_score(y_test, GB_y_pred)
-print(f"GB_Accuracy: {GB_accuracy}")       # 准确率为：0.5026584279350481
-"""
+print(f"GB_Accuracy: {GB_accuracy}")     # GB_accuracy：0.5026584279350481
+
     # 使用混淆矩阵来评估结果
 from sklearn.metrics import roc_curve, roc_auc_score, confusion_matrix
 import matplotlib.pyplot as plt
@@ -232,7 +219,6 @@ plt.xlabel('Predicted labels')
 plt.ylabel('True labels')
 plt.title('Confusion Matrix')
 plt.show()
-"""
 
 
 
@@ -240,20 +226,20 @@ plt.show()
 ## 2.7 Support Vector Machine（支持向量机）
 from sklearn.svm import SVC
     # 定义SVC模型
-#SVM_model = SVC(probability=True)
+SVM_model = SVC(probability=True)
     # 拟合模型
-#SVM_model.fit(X_train, y_train)
-# 保存模型
+SVM_model.fit(X_train, y_train)
+    # 保存模型   Сохраните модель
 import joblib
 #joblib.dump(SVM_model, 'saved_SVM_model.pkl')
-SVM_model = joblib.load('saved_SVM_model.pkl')
+#SVM_model = joblib.load('saved_SVM_model.pkl')
     # 预测
 SVM_y_pred = SVM_model.predict(X_test)
     # 计算准确率
 from sklearn.metrics import accuracy_score
 SVM_accuracy = accuracy_score(y_test, SVM_y_pred)
-print(f"SVM_Accuracy: {SVM_accuracy}")       # 准确率为：0.5765196148871964
-"""
+print(f"SVM_Accuracy: {SVM_accuracy}")       # SVM_accuracy：0.5765196148871964
+
     # 使用混淆矩阵来评估结果
 from sklearn.metrics import roc_curve, roc_auc_score, confusion_matrix
 import matplotlib.pyplot as plt
@@ -265,7 +251,6 @@ plt.xlabel('Predicted labels')
 plt.ylabel('True labels')
 plt.title('Confusion Matrix')
 plt.show()
-"""
 
 
 
@@ -273,20 +258,20 @@ plt.show()
 ## 2.8 Multilayer Perceptron（多层感知机）
 from sklearn.neural_network import MLPClassifier
     # 定义Multilayer Perceptron模型
-#MLP_model = MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=1000)
+MLP_model = MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=1000)
     # 拟合模型
-#MLP_model.fit(X_train, y_train)
-# 保存模型
+MLP_model.fit(X_train, y_train)
+    # 保存模型   Сохраните модель
 import joblib
 #joblib.dump(MLP_model, 'saved_MLP_model.pkl')
-MLP_model = joblib.load('saved_MLP_model.pkl')
+#MLP_model = joblib.load('saved_MLP_model.pkl')
     # 预测
 MLP_y_pred = MLP_model.predict(X_test)
     # 计算准确率
 from sklearn.metrics import accuracy_score
 MLP_accuracy = accuracy_score(y_test, MLP_y_pred)
-print(f"MLP_Accuracy: {MLP_accuracy}")       # 准确率为：0.5466302629688173
-"""
+print(f"MLP_Accuracy: {MLP_accuracy}")       # MLP_accuracy：0.5466302629688173
+
     # 使用混淆矩阵来评估结果
 from sklearn.metrics import roc_curve, roc_auc_score, confusion_matrix
 import matplotlib.pyplot as plt
@@ -298,7 +283,6 @@ plt.xlabel('Predicted labels')
 plt.ylabel('True labels')
 plt.title('Confusion Matrix')
 plt.show()
-"""
 
 
 
@@ -306,20 +290,20 @@ plt.show()
 ## 2.9 Ensemble methods（集成方法，如随机森林）
 from sklearn.ensemble import RandomForestClassifier
     # 定义RandomForest模型
-#RF_model = RandomForestClassifier(n_estimators=100)
+RF_model = RandomForestClassifier(n_estimators=100)
     # 拟合模型
-#RF_model.fit(X_train, y_train)
-# 保存模型
+RF_model.fit(X_train, y_train)
+    # 保存模型   Сохраните модель
 import joblib
 #joblib.dump(RF_model, 'saved_RF_model.pkl')
-RF_model = joblib.load('saved_RF_model.pkl')
+#RF_model = joblib.load('saved_RF_model.pkl')
     # 预测
 RF_y_pred = RF_model.predict(X_test)
     # 计算准确率
 from sklearn.metrics import accuracy_score
 RF_accuracy = accuracy_score(y_test, RF_y_pred)
-print(f"RF_Accuracy: {RF_accuracy}")       # 准确率为：0.568472481678402
-"""
+print(f"RF_Accuracy: {RF_accuracy}")       # RF_accuracy：0.568472481678402
+
     # 使用混淆矩阵来评估结果
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
@@ -331,7 +315,8 @@ plt.xlabel('Predicted labels')
 plt.ylabel('True labels')
 plt.title('Confusion Matrix')
 plt.show()
-"""
+
+
 
 
 """
@@ -340,16 +325,18 @@ new_text = str(input())
 # 对输入的文本进行预处理
 new_text_processed = pd.Series(new_text).str.lower()
 new_text_vectorized = vectorizer.transform(new_text_processed)
-# 使用训练好的模型进行情绪预测
+# 使用训练好的模型进行情绪预测(需要选择模型)
 predicted_emotion = model.predict(new_text_vectorized)[0]
 predicted_emotion_word = [key for key, value in emotion_mapping.items() if value == predicted_emotion][0]
 print(f"The predicted emotion for the input text is: {predicted_emotion_word}")
 """
 
 
-### 模型质量评估
 
-# 3.1 绘制ROC曲线图，评估模型质量    (roc_curve计算ROC曲线不支持多类别格式，必须是二分类才能使用)
+
+### 3 模型质量评估   Оценка качества моделей
+
+## 3.1 绘制ROC曲线图，评估模型质量    (roc_curve计算ROC曲线不支持多类别格式，必须是二分类才能使用)
 """
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
@@ -357,9 +344,8 @@ from sklearn.metrics import roc_curve, auc
 models = [LR_model, KNN_model, NB_model, DT_model, GB_model, SVM_model, MLP_model, RF_model]
 model_names = ['LR_model', 'KNN_model', 'NB_model', 'DT_model', 'GB_model', 'SVM_model', 'MLP_model', 'RF_model']
 
-# 假设X_test是测试数据，y_test是测试标签
+    # X_test是测试数据，y_test是测试标签
 fig, ax = plt.subplots(figsize=(10, 8))
-
 for model, name in zip(models, model_names):
     # 获取模型对测试集的预测概率
     y_scores = model.predict_proba(X_test)[:, 1]
@@ -382,7 +368,7 @@ plt.show()
 
 
 
-# 3.2 使用F1值评估模型
+## 3.2 使用F1值评估模型 Использование F1 для оценки моделей
 from sklearn.metrics import f1_score
 import matplotlib.pyplot as plt
 
@@ -417,11 +403,11 @@ plt.xlabel('Models')
 plt.ylabel('F1 Score')
 plt.title('F1 Score Comparison of Different Models')
 plt.show()
-"""
 
 
 
-# 3.3 使用准确值评估模型
+
+## 3.3 使用准确值评估模型 Использование точные значения для оценки моделей
 import matplotlib.pyplot as plt
 
 model_names = ['OneR','Logistic Regression', 'KNN', 'Naive Bayes', 'Decision Tree', 'Gradient Boosting', 'SVM', 'MLP', 'Random Forest']
@@ -433,4 +419,3 @@ plt.xlabel('Models')
 plt.ylabel('Accuracy')
 plt.title('Accuracy Comparison of Different Models')
 plt.show()
-"""
